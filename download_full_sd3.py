99c5502edd8fbfff19812efb76148fe5a97ae919
Dustin Avery <1998davery2020@gmail.com>
2024-06-13 18:43:29 -0500

    Getting basic stuff going for hugging face usage in python

---
+++ download_full_sd3.py
@@ -0,0 +1,14 @@
+from huggingface_hub import hf_hub_download
+
+# Comments as I understand them lol
+# Basic model no text
+hf_hub_download(repo_id="stabilityai/stable-diffusion-3-medium", filename="sd3_medium.safetensors", local_dir="models")
+
+# All in one but lower resource load
+hf_hub_download(repo_id="stabilityai/stable-diffusion-3-medium", filename="sd3_medium_incl_clips.safetensors", local_dir="models")
+
+# As above but more resource intensive
+hf_hub_download(repo_id="stabilityai/stable-diffusion-3-medium", filename="sd3_medium_incl_clips_t5xxlfp8.safetensors", local_dir="models")
+
+# Full model most resource intensive
+hf_hub_download(repo_id="stabilityai/stable-diffusion-3-medium", filename="sd3_medium_incl_clips_t5xxlfp16.safetensors", local_dir="models")
